884af43d81feb48557e13f59507c4a4236e0cea0
Luke Bodeen <luke_Bodeen@yahoo.com>
2017-01-09 22:15:05 -0600

    got algo working added tests

--- bin/nodesum.py
+++ bin/nodesum.py
@@ -9,44 +9,34 @@ class Node:
         self.nodeName = nodeName
         self.nodeValue = nodeValue
         self.nodeChildren = nodeChildren
-        self.pathSum = 0
+        self.pathSum = None
 #        print(self.nodeName)
 #        print(self.nodeValue)
     def __repr__(self):
-        return "<name: %s, value: %s>" % (self.nodeName, self.nodeValue)
+        return "<name: %s, value: %s, pathsum %s>" % (self.nodeName, self.nodeValue, self.pathSum)
 
 def getNodeName(incomingNode):
     return incomingNode.nodeName
 
 class Tree:
     
-    TreeList = []
-    
+    TreeList = []    
     def add(self, nodeToAdd):
         self.TreeList.append(nodeToAdd) 
-#        print(self.TreeList)
     def sortTree(self):
         sorted(self.TreeList, key=getNodeName)
     def getIndexOfNodeMember(self, incommingNodeName):
-        print("Tree list inside get index")
-        print(self.TreeList)
+#         print(self.TreeList)
         for i in range(self.TreeList.__len__()):
             if self.TreeList[i].nodeName == incommingNodeName:
                 return i
         return None
     
 def doesSumExist(startNode, sumTarget, mytree):
-#    print(sumTarget)
+
     currentSum = 0
     pastStartPoint = False
-#     currentDepth = 0
-#     maxdepth = 10
-#     treeTotalNodes = mytree.TreeList.__len__()
-#    print(treeTotalNodes)
-    childrenRemain = True
     trimmedTree = copy.deepcopy(mytree)
-#     tempTree.TreeList = sorted(tempTree.TreeList, key=getNodeName)
-    print(trimmedTree.TreeList)
     
     #while tree list is not totally popped
     while trimmedTree.TreeList:    
@@ -56,36 +46,20 @@ def doesSumExist(startNode, sumTarget, mytree):
             pastStartPoint = True
             currentSum = trimmedTree.TreeList[nodeIterator].pathSum = trimmedTree.TreeList[nodeIterator].nodeValue   
             if(trimmedTree.TreeList[nodeIterator].pathSum == sumTarget):
-                break
-            if(trimmedTree.TreeList[nodeIterator].nodeChildren):
-                if(CheckChildrenForSum(trimmedTree, nodeIterator, currentSum, sumTarget)):
-                    print("FOUND PATH!!!")
-                    break
-            trimmedTree.TreeList.pop(nodeIterator) 
-            nodeIterator += 1           
-            print(trimmedTree.TreeList)
-            
+                return True
+        
         elif(pastStartPoint):
-            print(trimmedTree.TreeList[nodeIterator].nodeName)
-            currentSum = trimmedTree.TreeList[nodeIterator].pathSum = trimmedTree.TreeList[nodeIterator].nodeValue + currentSum
-            print("current sum past start")
-            print(currentSum)
-            if(currentSum == sumTarget):
-                break
-            if(trimmedTree.TreeList[nodeIterator].nodeChildren):
-                if(CheckChildrenForSum(trimmedTree, nodeIterator, currentSum, sumTarget)):
-                    print("FOUND PATH!!!")
-                    return True
-            trimmedTree.TreeList.pop(nodeIterator)
-            nodeIterator += 1
-            print(trimmedTree.TreeList)
-        else:
-            #skip any node before start point
-            trimmedTree.TreeList.pop(nodeIterator)
-            nodeIterator += 1
-            print(trimmedTree.TreeList)                   
 
-    return (currentSum == sumTarget)
+            if(trimmedTree.TreeList[nodeIterator].pathSum == sumTarget):
+                return True
+   
+        if(pastStartPoint and trimmedTree.TreeList[nodeIterator].nodeChildren):
+            if(CheckChildrenForSum(trimmedTree, nodeIterator, currentSum, sumTarget)):
+                return True
+        #skip any node before start point or advance to next node
+        trimmedTree.TreeList.pop(nodeIterator)
+        nodeIterator += 1       
+    return False
 
 def CheckChildrenForSum(trimmedTree, nodeIterator, currentSum, sumTarget):
     for childIterator in range(trimmedTree.TreeList[nodeIterator].nodeChildren.__len__()):
@@ -93,11 +67,10 @@ def CheckChildrenForSum(trimmedTree, nodeIterator, currentSum, sumTarget):
 #                     print(mytree.TreeList[nodeIterator].nodeChildren[childIterator])
 #                     print("child index")
         childIndex = trimmedTree.getIndexOfNodeMember(trimmedTree.TreeList[nodeIterator].nodeChildren[childIterator])
-        trimmedTree.TreeList[childIndex].pathSum = trimmedTree.TreeList[childIndex].nodeValue + currentSum
+        trimmedTree.TreeList[childIndex].pathSum = trimmedTree.TreeList[childIndex].nodeValue + trimmedTree.TreeList[nodeIterator].pathSum
         if(trimmedTree.TreeList[childIndex].pathSum == sumTarget):
-            print(trimmedTree.TreeList[childIndex].nodeName)
-            print("FOUND PATH INSIDE CHILDREN SUM!!!!!!!!")
-            return trimmedTree.TreeList[childIndex].pathSum     
+#             print(trimmedTree.TreeList[childIndex].nodeName)
+            return childIndex   
     return False
 
 mytree = Tree()
@@ -116,16 +89,23 @@ mytree.add(nodef)
 nodea = Node("a", 5, ["b", "c", "d"])
 mytree.add(nodea)
 mytree.TreeList = sorted(mytree.TreeList, key=getNodeName)
-
-# print(" 4 test ")
-# print(doesSumExist("a", 4, mytree))
-# print(" 5 test ")
-# print(doesSumExist("a", 5, mytree))
-print(" 12 test ")
+ 
+print("a4 test ")
+print(doesSumExist("a", 4, mytree))
+print("d2 test ")
+print(doesSumExist("d", 2, mytree))
+print("d3 test ")
+print(doesSumExist("d", 3, mytree))
+print("a5 test ")
+print(doesSumExist("a", 5, mytree))
+print("a12 test ")
 print(doesSumExist("a", 12, mytree))
-# 
-# print(" 21 test ")
-# print(doesSumExist("a", 21, mytree))
+print("a21 test ")
+print(doesSumExist("a", 21, mytree))
+print("c3 test ")
+print(doesSumExist("c", 3, mytree))
+print("d16 test ")
+print(doesSumExist("d", 16, mytree))
 
 
     
